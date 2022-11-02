b3f84a287e0d486ec6bfdaa041e445119548c27a
FrancescoSepe98 <sepe.francesco.lavoro@gmail.com>
2022-11-02 19:11:56 +0100

    Creation and testing  of the class Student and Teacher

---
+++ src/it/develope/VariablesMethodsConstuctor02/Student.java
@@ -0,0 +1,14 @@
+package it.develope.VariablesMethodsConstuctor02;
+
+public class Student {
+    public String name;
+    public int grade;
+
+    public Student(String studentName){
+        System.out.println("A Student object has been created!\n");
+        this.name=studentName;
+    }
+    public void getStudentDetails(){
+        System.out.println("Student name:" + name + " " + "student grade:" + grade);
+    }
+}
